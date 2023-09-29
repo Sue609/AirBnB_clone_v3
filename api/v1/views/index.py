3cc55285d4bbc4a6e75f2e604e7c06425ed95ae5
SAMKE-glitch <sammythemwa@gmail.com>
2023-09-29 07:35:54 -0700

    some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,12 +2,26 @@
 """
 Index file
 """
-
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
 
+"""
 @app_views.route('/status', methods=['GET'])
 def status():
-    """Returns JSON status"""
-    return jsonify({"status": "OK"})
+    Returns JSON status
+    return jsonify({"status": "OK"})"""
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    """Retrieves the number of each object by type"""
+    object_count = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+    return jsonify(object_count)
