07d9fe5adcdef7feeb10d7a65d621f35aad84335
SAMKE-glitch <sammythemwa@gmail.com>
2023-09-29 07:06:32 -0700

    implememnted checking status

---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Index file
+"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """Returns JSON status"""
+    return jsonify({"status": "OK"})
