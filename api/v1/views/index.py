eb6688979219a04ee690740476ed92505947493b
SAMKE-glitch <sammythemwa@gmail.com>
2023-09-29 07:39:45 -0700

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,11 +7,10 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-"""
 @app_views.route('/status', methods=['GET'])
 def status():
-    Returns JSON status
-    return jsonify({"status": "OK"})"""
+    """Returns JSON status"""
+    return jsonify({"status": "OK"})
 
 @app_views.route('/stats', methods=['GET'])
 def count():
